3544cab50983ecee3e4dee52ac376e84e6da7810
Gopi Saraswat <126561948+GopiSarswat@users.noreply.github.com>
2026-06-09 10:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (81.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,9 +4,7 @@ public:
         int j = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                int x = nums[i];
-                nums[i] = nums[j];
-                nums[j] = x;
+                swap(nums[i], nums[j]);
                 j++;
             }
         }
