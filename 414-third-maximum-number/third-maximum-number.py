cf4ef2e9f3d12d674baf5965be9b21ea9b84371d
Gopi Saraswat <126561948+GopiSarswat@users.noreply.github.com>
2026-06-23 11:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (88.27%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        top = []
+        for num in nums:
+            if num in top:
+                continue
+            top.append(num)
+            top.sort(reverse=True)
+            if len(top) > 3:
+                top.pop()
+        return top[2] if len(top) == 3 else top[0]
